876b98f7a2cd828042edcd4ced633058b2a12ef4
jinyongyun <yunjinyong7302000@gmail.com>
2025-03-10 21:37:08 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,7 +1,8 @@
 def solution(my_string):
     
-    stringlist = list(my_string)
-    stringlist.reverse()
-    answer = ''.join(stringlist)
+    #stringlist = list(my_string)
+    #stringlist.reverse()
+    #answer = ''.join(stringlist)
+    answer = ''.join(reversed(my_string))
     
     return answer
